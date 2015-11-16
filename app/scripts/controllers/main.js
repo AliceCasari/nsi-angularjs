f64b4472ac013cab21c7a5cb8cdc076f4c1a4bbf
acasari <casalialice@gmail.com>
2015-11-16 13:02:11 +0100

    wip

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularnewcourseApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularnewcourseApp
+ */
+angular.module('angularnewcourseApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
