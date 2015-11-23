f6c6490dceec88925aad4ade7d665a6464c68977
acasari <casalialice@gmail.com>
2015-11-23 12:16:50 +0100

    wip

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,6 +29,11 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/stanze', {
+        templateUrl: 'views/stanze.html',
+        controller: 'StanzeCtrl',
+        controllerAs: 'stanze'
+      })
       .when('/login', {
         templateUrl: 'views/login.html',
         controller: 'LoginCtrl',
@@ -47,8 +52,9 @@ angular
 RunModule.$inject = ['$rootScope', '$location', 'UtenteFactory'];
 function RunModule($rootScope, $location, UtenteFactory) {
   $rootScope.$on('$routeChangeStart', function(event, next, current) {
-    console.log(next.$$route.originalPath.indexOf('login'), UtenteFactory.getUtente.data.Cognome);
+    $rootScope.activePage = next.$$route.originalPath;
     if(next.$$route.originalPath.indexOf('login') === -1 && !UtenteFactory.getUtente.data.Cognome){
+      $rootScope.activePage = '/login';
       $location.path('/login');
     }
   });
---
+++ app/scripts/controllers/stanze.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularnewcourseApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularnewcourseApp
+ */
+angular.module('angularnewcourseApp')
+  .controller('StanzeCtrl', function () {
+    var vm = this;
+    vm.listaStanze = [];
+
+    activate();
+
+    function activate() {
+
+    }
+  });
