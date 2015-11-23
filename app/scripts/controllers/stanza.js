d3135d857f13afc6804b5ee07b0ea1429b60e0b6
acasari <casalialice@gmail.com>
2015-11-23 15:26:02 +0100

    wip

--- app/scripts/controllers/stanza.js
+++ app/scripts/controllers/stanza.js
@@ -20,6 +20,21 @@ angular.module('angularnewcourseApp')
           .then(
           function(data) {
             vm.stanzaDettaglio = data.data;
+
+            var now = new Date(new Date().setHours(0,0,0,0));
+            var tomorrow = new Date(new Date().setHours(24,0,0,0));
+            $http.get(baseUrl + '/api/Stanza/' + $routeParams.id + '/PeriodiStato',
+              {params: {inizio: now, fine: tomorrow}}
+            )
+              .then(
+                function(data) {
+                  console.log('stato stanza', data);
+                  vm.stanzaDettaglio.Stato = data.data;
+                },
+                function(error) {
+                  alert('error');
+                }
+              );
           },
           function(error) {
             alert('error');
